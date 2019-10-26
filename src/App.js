f6e9f7fa2ed5f68f56b3bda07e7e73d515fc18b9
Joey Imlay <jo.imlay@gmail.com>
2019-10-26 15:21:04 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TapCard from './TapCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tapchat</h1>
+      <div className="tapboard">
+        <TapCard word="yo" />
+        <TapCard word="we" />
+        <TapCard word="are" />
+        <TapCard word="here" />
+      </div>
     </div>
   );
 }
