d3d54e29671040fa4c31a720ca23b25b47784c41
Joey Imlay <jo.imlay@gmail.com>
2019-10-27 12:02:13 +0000

    Updates

---
+++ precache-manifest.4cd8256d2feaf83ade0b7d414f139d6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0a31d722f1872a8b19f2af99c34877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6507830bf2c48f787839",
+    "url": "/static/css/main.331030dc.chunk.css"
+  },
+  {
+    "revision": "ab3c1838ab485d414cb4",
+    "url": "/static/js/2.e05e0a0b.chunk.js"
+  },
+  {
+    "revision": "6507830bf2c48f787839",
+    "url": "/static/js/main.28f556ba.chunk.js"
+  },
+  {
+    "revision": "6da4ffd814d0199d8132",
+    "url": "/static/js/runtime-main.60340576.js"
+  },
+  {
+    "revision": "be1e6672908f2d76091de9ac60983c89",
+    "url": "/static/media/OpenDyslexic-Regular.be1e6672.otf"
+  },
+  {
+    "revision": "5dc7233a326ad2bebdca89418e96264c",
+    "url": "/static/media/blackwidow.5dc7233a.jpg"
+  },
+  {
+    "revision": "4f13c40dfd2205a91ffd4fb05a129842",
+    "url": "/static/media/captainamerica.4f13c40d.webp"
+  },
+  {
+    "revision": "4d4b260eaaf8a2354f01b8388b7548f8",
+    "url": "/static/media/ironman.4d4b260e.jpg"
+  }
+]);
