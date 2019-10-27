876beef8018e42cf9e07781762532ad504a19f7a
Joey Imlay <jo.imlay@gmail.com>
2019-10-27 12:08:04 +0000

    Updates

---
+++ precache-manifest.24bf5f4d5005537fb73fdda15cf3edee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f628d8710d11c9ce44a79047bc449f19",
+    "url": "/tapchat-ui/index.html"
+  },
+  {
+    "revision": "fdc7109cb6b3b1863a57",
+    "url": "/tapchat-ui/static/css/main.28ed090a.chunk.css"
+  },
+  {
+    "revision": "ab3c1838ab485d414cb4",
+    "url": "/tapchat-ui/static/js/2.e05e0a0b.chunk.js"
+  },
+  {
+    "revision": "fdc7109cb6b3b1863a57",
+    "url": "/tapchat-ui/static/js/main.fd924c72.chunk.js"
+  },
+  {
+    "revision": "dc7837e6e73d2527f764",
+    "url": "/tapchat-ui/static/js/runtime-main.453b0e6a.js"
+  },
+  {
+    "revision": "be1e6672908f2d76091de9ac60983c89",
+    "url": "/tapchat-ui/static/media/OpenDyslexic-Regular.be1e6672.otf"
+  },
+  {
+    "revision": "5dc7233a326ad2bebdca89418e96264c",
+    "url": "/tapchat-ui/static/media/blackwidow.5dc7233a.jpg"
+  },
+  {
+    "revision": "4f13c40dfd2205a91ffd4fb05a129842",
+    "url": "/tapchat-ui/static/media/captainamerica.4f13c40d.webp"
+  },
+  {
+    "revision": "4d4b260eaaf8a2354f01b8388b7548f8",
+    "url": "/tapchat-ui/static/media/ironman.4d4b260e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4cd8256d2feaf83ade0b7d414f139d6a.js"
+  "/tapchat-ui/precache-manifest.24bf5f4d5005537fb73fdda15cf3edee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tapchat-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
